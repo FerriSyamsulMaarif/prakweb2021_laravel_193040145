05e06cf581af8aa93fdacd96d5a2aa535431c9d8
FerriSyamsulMaarif <193040145.ferry@mail.unpas.ac.id>
2021-10-08 17:15:34 +0700

    Membuah folder-folder baru seperti Posts,post dan lain yang akan membuah atau memanggil dari kelas induknya

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,90 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Ferri Syamsul Ma'arif",
         "email" => "193040145.ferry@mail.unpas.ac.id",
         "image" => "193040145.jpg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Ferri Syamsul Ma'arif",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quia laborum et quo 
+            tempora harum nesciunt eaque nulla rerum dolorum voluptates quae atque a sint quibusdam doloremque
+             iste fugit veniam itaque, dolores nostrum provident commodi sunt accusantium? Officia consequuntur 
+             incidunt voluptate debitis, velit vitae quaerat voluptatem. Eaque delectus officia, fugiat, corporis 
+             ab voluptate odit atque cum natus commodi recusandae sed nihil aspernatur corrupti incidunt ratione maiores 
+            vitae? A, ex officia vero magni deserunt dignissimos, beatae minus, illum et totam doloremque."
+        ],
+    
+        [
+            "title" => "Judul Post kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Dulmin",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quia laborum et quo 
+            tempora harum nesciunt eaque nulla rerum dolorum voluptates quae atque a sint quibusdam doloremque
+             iste fugit veniam itaque, dolores nostrum provident commodi sunt accusantium? Officia consequuntur 
+             incidunt voluptate debitis, velit vitae quaerat voluptatem. Eaque delectus officia, fugiat."
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+// halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Ferri Syamsul Ma'arif",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quia laborum et quo 
+            tempora harum nesciunt eaque nulla rerum dolorum voluptates quae atque a sint quibusdam doloremque
+             iste fugit veniam itaque, dolores nostrum provident commodi sunt accusantium? Officia consequuntur 
+             incidunt voluptate debitis, velit vitae quaerat voluptatem. Eaque delectus officia, fugiat, corporis 
+             ab voluptate odit atque cum natus commodi recusandae sed nihil aspernatur corrupti incidunt ratione maiores 
+            vitae? A, ex officia vero magni deserunt dignissimos, beatae minus, illum et totam doloremque."
+        ],
+    
+        [
+            "title" => "Judul Post kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Dulmin",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quia laborum et quo 
+            tempora harum nesciunt eaque nulla rerum dolorum voluptates quae atque a sint quibusdam doloremque
+             iste fugit veniam itaque, dolores nostrum provident commodi sunt accusantium? Officia consequuntur 
+             incidunt voluptate debitis, velit vitae quaerat voluptatem. Eaque delectus officia, fugiat."
+        ],
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });

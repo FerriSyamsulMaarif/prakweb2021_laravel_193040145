ad8ce949d28790c0a020c465c03b28f897f238b2
FerriSyamsulMaarif <193040145.ferry@mail.unpas.ac.id>
2021-10-16 21:47:12 +0700

    Melanjutkan kembali video ke 8 yang membahas mengenai eloquent relationship yaitu mengelola relasi table

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2>{{ $post->title }}</h2>
+    
+        <h2 class="mb-5">{{ $post->title }}</h2>
+
+        <p>By. Ferri Syamsul Maarif in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
         {!! $post->body !!}
-    </article>
+    
     
     <a href="/posts">Back to Posts</a>
 @endsection

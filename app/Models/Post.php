4edaec4c21d159562183474814e17ce9d646a6ac
FerriSyamsulMaarif <193040145.ferry@mail.unpas.ac.id>
2021-10-22 17:36:21 +0700

    Membuat beberapa fitur seeder pada video ke 9

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Post extends Model
 {
@@ -13,7 +15,13 @@ class Post extends Model
 
     protected $guarded = ['id'];
 
-    public function category() {
+    public function category() 
+    {
         return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,107 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Ferri Syamsul M',
+            'email' => '193040145.ferry@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Denggo;',
+            'email' => 'Denggol@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programing',
+            'slug' => 'web-programing'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Velit soluta maxime, maiores, magnam tempore porro ullam iusto eum',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Velit soluta maxime, maiores, magnam tempore porro ullam iusto eum id 
+            esse eligendi inventore quod quia dicta aliquam non officiis in accusamus 
+            corrupti odit expedita, repudiandae quae! Eveniet vel non eligendi, nesciunt 
+            libero earum in repellendus iusto, nihil, velit ducimus perspiciatis 
+            expedita at temporibus aliquam. Aliquid nobis quo dicta voluptatibus 
+            debitis accusamus inventore fugit est. Dolorem maxime hic libero, qui 
+            at molestias architecto nemo itaque autem recusandae optio molestiae 
+            veniam doloremque blanditiis earum placeat dicta quidem laboriosam magni, 
+            deleniti cum illo, reiciendis dolor. Ducimus ipsum vitae esse maiores aut, 
+            odio at exercitationem.',
+            'category-id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Velit soluta maxime, maiores, magnam tempore porro ullam iusto eum',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Velit soluta maxime, maiores, magnam tempore porro ullam iusto eum id 
+            esse eligendi inventore quod quia dicta aliquam non officiis in accusamus 
+            corrupti odit expedita, repudiandae quae! Eveniet vel non eligendi, nesciunt 
+            libero earum in repellendus iusto, nihil, velit ducimus perspiciatis 
+            expedita at temporibus aliquam. Aliquid nobis quo dicta voluptatibus 
+            debitis accusamus inventore fugit est. Dolorem maxime hic libero, qui 
+            at molestias architecto nemo itaque autem recusandae optio molestiae 
+            veniam doloremque blanditiis earum placeat dicta quidem laboriosam magni, 
+            deleniti cum illo, reiciendis dolor. Ducimus ipsum vitae esse maiores aut, 
+            odio at exercitationem.',
+            'category-id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Velit soluta maxime, maiores, magnam tempore porro ullam iusto eum',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Velit soluta maxime, maiores, magnam tempore porro ullam iusto eum id 
+            esse eligendi inventore quod quia dicta aliquam non officiis in accusamus 
+            corrupti odit expedita, repudiandae quae! Eveniet vel non eligendi, nesciunt 
+            libero earum in repellendus iusto, nihil, velit ducimus perspiciatis 
+            expedita at temporibus aliquam. Aliquid nobis quo dicta voluptatibus 
+            debitis accusamus inventore fugit est. Dolorem maxime hic libero, qui 
+            at molestias architecto nemo itaque autem recusandae optio molestiae 
+            veniam doloremque blanditiis earum placeat dicta quidem laboriosam magni, 
+            deleniti cum illo, reiciendis dolor. Ducimus ipsum vitae esse maiores aut, 
+            odio at exercitationem.',
+            'category-id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Velit soluta maxime, maiores, magnam tempore porro ullam iusto eum',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Velit soluta maxime, maiores, magnam tempore porro ullam iusto eum id 
+            esse eligendi inventore quod quia dicta aliquam non officiis in accusamus 
+            corrupti odit expedita, repudiandae quae! Eveniet vel non eligendi, nesciunt 
+            libero earum in repellendus iusto, nihil, velit ducimus perspiciatis 
+            expedita at temporibus aliquam. Aliquid nobis quo dicta voluptatibus 
+            debitis accusamus inventore fugit est. Dolorem maxime hic libero, qui 
+            at molestias architecto nemo itaque autem recusandae optio molestiae 
+            veniam doloremque blanditiis earum placeat dicta quidem laboriosam magni, 
+            deleniti cum illo, reiciendis dolor. Ducimus ipsum vitae esse maiores aut, 
+            odio at exercitationem.',
+            'category-id' => 2,
+            'user_id' => 2
+        ]);
     }
 }

355a7133caa5ec4acee39f91cdfb15645bf965ad
FerriSyamsulMaarif <193040145.ferry@mail.unpas.ac.id>
2021-10-29 20:35:58 +0700

    Melanjutkan kembali video ke 13 yang membahas mengenai searching & pagination

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -75,33 +75,5 @@
       {{ $posts->links() }}
     </div>
     
-
-
-    {{-- <div class="container">
-      <div class="row">
-          @foreach ($posts->skip(1) as $post)
-              <div class="col-md-4 mb-3">
-                  <div class="card">
-                      <div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/posts?category={{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
-                      <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
-                      <div class="card-body">
-                      <h5 class="card-title">{{ $post->title }}</h5>
-                      <p>
-                          <small class="text-muted">By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
-                          </small>
-                      </p>
-                      <p class="card-text">{{ $post->excerpt }}</p>
-                      <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
-                      </div>
-                  </div>
-              </div>
-          @endforeach
-      </div>
-  </div>
-
-  @else
-      <p class="text-center fs-4">No Post Found.</p>
-  @endif --}}
-
 @endsection
     
